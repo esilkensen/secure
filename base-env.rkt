621ece19376b1837bed4b496256b005864334cc1
Erik Silkensen <silkense@colorado.edu>
2012-09-04 23:07:19 -0400

    remove build-name

--- base-env.rkt
+++ base-env.rkt
@@ -5,21 +5,12 @@
 (require "label.rkt")
 
 (define-syntax (define/secure stx)
-  (define (build-name id . parts)
-    (let ([str (apply string-append
-                      (map (λ (p)
-                             (if (syntax? p)
-                                 (symbol->string (syntax-e p))
-                                 (format "~a" p)))
-                           parts))])
-      (datum->syntax id (string->symbol str) id)))
   (syntax-case stx ()
     [(_ id kw-formals body . rest)
      (let ([proc-datum `(,#'λ ,#'kw-formals (@⊥ ,#'body . ,#'rest))])
        (with-syntax ([proc (syntax-property
                             (datum->syntax stx proc-datum stx stx)
-                            'inferred-name (syntax-e #'id))]
-                     [secure-id (build-name #'id "secure-" #'id)])
+                            'inferred-name (syntax-e #'id))])
          #`(begin
              (provide (rename-out [secure-id id]))
              (define secure-id (@⊥ proc)))))]))
